98b9f2f5415542be512f0270901cfaa4bd3baf2e
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-24 18:57:47 -0500

    ran through Lauren's coding

--- main.js
+++ main.js
@@ -3,35 +3,67 @@ const printToDom = (stringToPrint, divId) => {
     selectedDiv.innerHTML += stringToPrint;
 };
 
-// Functions for 1st text box //
+// Commented out below was my original solution which was way more complex and convuluted. Lauren's example is listed bottom //
 
-const output = '';
+// // Functions for 1st text box //
 
-const textOutput = () => {
-    const inputText = document.getElementById('textAreaOne').value;
-    let inputWords = inputText.split('');
-    for (let i = 0; i < inputWords.length; i++) {
-        let output = inputWords[i];
-    }
-    printToDom(inputText, 'wordOutput');
+// const output = '';
+
+// const textOutput = () => {
+//     const inputText = document.getElementById('textAreaOne').value;
+//     let inputWords = inputText.split('');
+//     for (let i = 0; i < inputWords.length; i++) {
+//         let output = inputWords[i];
+//     }
+//     printToDom(inputText, 'wordOutput');
+// };
+
+// const secretButton = document.getElementById('secret-button-one');
+// secretButton.addEventListener("click", textOutput);
+
+// // Functions for 2nd text box //
+
+// const outputTwo = '';
+
+// const secondOutput = () => {
+//     const secondInputText = document.getElementById('textAreaTwo').value;
+//     let secondInputWords = secondInputText.split(' ');
+//     for (let i = 0; i < secondInputWords.length; i++) {
+//         let outputTwo = secondInputWords[i];
+//     }
+//     printToDom(secondInputText, 'wordOutputTwo');
+// };
+
+// const secondSecretButton = document.getElementById('secret-button-two');
+// secondSecretButton.addEventListener("click", secondOutput);
+
+// Lauren's example //
+
+const convertToCode = () => {
+        const words = document.getElementById('textAreaOne').value;
+        let converted = '';
+        words.split('').forEach((character, i) => {
+            converted += (words.length === i + 1) ? `${character.charCodeAt(0)}` 
+            : `${character.charCodeAt(0)},`;
+    })
+    printToDom(converted, 'wordOutput');
 };
 
-const secretButton = document.getElementById('secret-button-one');
-secretButton.addEventListener("click", textOutput);
+const convertToWords = () => {
+    const codeList = document.getElementById('textAreaTwo').value;
+    let text = '';
+    codeList.split(',').forEach((code) => {
+       text += String.fromCharCode(code);
+    });
+    printToDom(text, 'wordOutputTwo');
+}
 
-// Functions for 2nd text box //
+const registerEvents = () => {
+    document.getElementById('secret-button-one').addEventListener('click', convertToCode);
+    document.getElementById("secret-button-two").addEventListener('click', convertToWords);
+}
 
-const outputTwo = '';
+registerEvents();
 
-const secondOutput = () => {
-    const secondInputText = document.getElementById('textAreaTwo').value;
-    let secondInputWords = secondInputText.split(' ');
-    for (let i = 0; i < secondInputWords.length; i++) {
-        let outputTwo = secondInputWords[i];
-    }
-    printToDom(secondInputText, 'wordOutputTwo');
-};
-
-const secondSecretButton = document.getElementById('secret-button-two');
-secondSecretButton.addEventListener("click", secondOutput);
-
+// terniary operator
+// (conditional statement) ? true condition and : false condition
